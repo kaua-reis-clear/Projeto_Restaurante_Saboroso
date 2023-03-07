58014edbea5e30b7274af26c52a3dc24aa24bafd
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-07 12:55:41 -0300

    Projeto_Saboroso: added methods to validate fields and sign in on admin interface.

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
-var express = require('express');
+var express = require("express");
+var users = require("./../inc/users")
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
@@ -7,11 +8,24 @@ router.get("/", function(req, res, next) {
     });
 });
 
-router.get("/login", function(req, res, next) {
-    if (!req.session.views) req.session.views = 0;
+router.post("/login", function(req, res, next) {
+    if(!req.body.email) {
+        users.render(req, res, "Preencha o campo e-mail.");
+    } else if (!req.body.password) {
+        users.render(req, res, "Preencha o campo senha");
+    } else {
+        users.login(req.body.email, req.body.password).then(user => {
+            req.session.user = user;
 
-    console.log("SESSION:", req.session.views++);
-    res.render("admin/login");
+            res.redirect("/admin");
+        }).catch(err => {
+            users.render(req, res, err.message || err);
+        })
+    }
+});
+
+router.get("/login", function(req, res, next) {
+    users.render(req, res, null)
 });
 
 router.get("/contacts", function(req, res, next) {
