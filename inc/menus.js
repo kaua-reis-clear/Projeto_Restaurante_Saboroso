0ab7236d98ed3b92f050a893b12789e86a990389
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-08 13:00:04 -0300

    Projeto_Saboroso: updating data in mysql by post method.

--- inc/menus.js
+++ inc/menus.js
@@ -7,11 +7,10 @@ module.exports = {
       conn.query(
         `
             SELECT * FROM tb_menus ORDER BY title
-            
-        `,
+            `,
         (err, results) => {
           if (err) {
-            console.log(err);
+            reject(err);
           }
           resolve(results);
         }
@@ -21,22 +20,44 @@ module.exports = {
 
   save(fields, files) {
     return new Promise((resolve, reject) => {
-      fields.photo = `images/${path.parse(files.photo.path).base}`
-      conn.query(`
-        INSERT INTO tb_menus (title, description, price, photo)
-        VALUES(?, ?, ?, ?)
-      `, [
-        fields.title,
-        fields.description,
-        fields.price,
-        fields.photo
-      ], (err, results) => {
-        if(err) {
+      fields.photo = `images/${path.parse(files.photo.path).base}`;
+
+      let query,
+        queryPhoto = "",
+        params = [fields.title, fields.description, fields.price];
+
+      if (files.photo.name) {
+        queryPhoto = ",photo = ?";
+        params.push(fields.photo);
+      }
+
+      if (parseInt(fields.id) > 0) {
+        params.push(fields.id);
+        query = `
+        UPDATE tb_menus
+        SET title = ?,
+        description = ?,
+        price = ?
+        ${queryPhoto}
+        WHERE id = ?
+        `;
+      } else {
+        if (!files.photo.name) {
+          reject("Envie a foto do prato.");
+        }
+        query = `
+          INSERT INTO tb_menus (title, description, price, photo)
+          VALUES(?, ?, ?, ?)
+        `;
+      }
+
+      conn.query(query, params, (err, results) => {
+        if (err) {
           reject(err);
         } else {
           resolve(results);
         }
       });
     });
-  }
+  },
 };
