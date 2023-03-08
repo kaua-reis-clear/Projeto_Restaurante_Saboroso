da93831eb0632ca93e4f7ef9b69287a3ea0cb617
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-08 08:21:30 -0300

    Projeto_Saboroso: created middleware in order to validate login session.

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,23 @@ var express = require("express");
 var users = require("./../inc/users")
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-    res.render("admin/index", {
 
-    });
+router.use(function(req, res, next) {
+    if(['/login'].indexOf(req.url) === -1 && !req.session.user) {
+        res.redirect("/admin/login");
+    } else {
+        next();
+    }
+});
+
+router.get("/logout", function(req, res, next) {
+    delete req.session.user;
+
+    res.redirect("/admin/login")
+})
+
+router.get("/", function(req, res, next) {
+    res.render("admin/index");
 });
 
 router.post("/login", function(req, res, next) {
