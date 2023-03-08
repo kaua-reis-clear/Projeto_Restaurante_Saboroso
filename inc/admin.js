4de95d47483da627a514ae80cd438684ce2e89a0
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-08 09:16:36 -0300

    Projet_Saboroso: getting user data from mysql and displaying it on screen.

--- inc/admin.js
+++ inc/admin.js
@@ -1,4 +1,11 @@
 module.exports = {
+    getParams(req, params) {
+        return Object.assign({}, {
+            menus: req.menus,
+            user: req.session.user
+        });
+    },
+    
     getMenus(req){
         let menus = [
             {
