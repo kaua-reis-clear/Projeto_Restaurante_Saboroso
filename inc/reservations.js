2adc4dc1573a31f2f2e3ed028b68123b53e795b6
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-09 09:09:22 -0300

    Projeto_Saboroso: created methods to insert data into reservations table and dispaying it on admin page.

--- inc/reservations.js
+++ inc/reservations.js
@@ -14,26 +14,61 @@ module.exports = {
 
   save(fields) {
     return new Promise((resolve, reject) => {
+      if(fields.date.indexOf('/') > -1) {
         let date = fields.date.split('/');
+        fields.date = `${date[2]}-${date[1]}-${date[0]}`;
+      }
 
-        fields.date = `${date[2]}-${date[1]}-${date[0]}`
+      let query, params = [
+        fields.name,
+        fields.email,
+        fields.people,
+        fields.date,
+        fields.time
+      ];
+    
+      if(parseInt(fields.id) > 0) {
+        query =  `
+          UPDATE tb_reservations
+          SET
+            name = ?,
+            email = ?,
+            people = ?,
+            date = ?,
+            time = ?
+          WHERE id = ?
+        `;
+
+        params.push(fields.id);
+      } else {
+        query = `
+          INSERT INTO tb_reservations (name, email, people, date, time)
+          VALUES(?, ?, ?, ?, ?)
+          `;
+      }
+      conn.query(query, params, (err, results) => {
+          if(err) {
+              reject(err);
+          } else {
+              resolve(results);
+          }
+      });
+    });
+  },
+
+  getReservations() {
+    return new Promise((resolve, reject) => {
       conn.query(
         `
-            INSERT INTO tb_reservations (name, email, people, date, time)
-            VAlues(?, ?, ?, ?, ?)
-        `, [
-            fields.name,
-            fields.email,
-            fields.people,
-            fields.date,
-            fields.time
-        ], (err, results) => {
-            if(err) {
-                reject(err);
-            } else {
-                resolve(results);
-            }
-        });
+        SELECT * FROM tb_reservations ORDER BY date DESC
+        `,
+        (err, results) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(results);
+        }
+      );
     });
   },
 };
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require("express");
 var users = require("./../inc/users");
 var admin = require("./../inc/admin");
 var menus = require("./../inc/menus");
+var reservations = require("./../inc/reservations");
 var router = express.Router();
 
 
@@ -85,12 +86,31 @@ router.delete("/menus/:id", function(req, res, next) {
     }).catch(err => {
         res.send(err);
     });
-})
+});
 
 router.get("/reservations", function(req, res, next) {
-    res.render("admin/reservations", admin.getParams(req, {
-        date: {}
-    }));
+    reservations.getReservations().then(data => {
+        res.render("admin/reservations", admin.getParams(req, {
+            date: {},
+            data
+        }));
+    });
+});
+
+router.post("/reservations", function(req, res, next) {
+    reservations.save(req.fields, req.files).then(results => {
+        res.send(results);
+    }).catch(err => {
+        res.send(err);
+    });
+});
+
+router.delete("/reservations/:id", function(req, res, next) {
+    reservations.delete(req.params.id).then(results => {
+        res.send(results);
+    }).catch(err => {
+        res.send(err);
+    });
 });
 
 router.get("/users", function(req, res, next) {
