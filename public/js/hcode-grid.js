6f378c436349482ca0b54839f5e9c3c4e337d5ae
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-09 10:49:12 -0300

    Projeto_Saboroso: delete and edit events refactoreds.

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -1,5 +1,26 @@
 class HcodeGrid {
     constructor(configs) {
+        configs.listeners = Object.assign({
+            afterUpdateClick: (e) => {
+              $("#modal-update").modal("show");
+              },
+            afterDeleteClick: (e) => {
+              window.location.reload();
+            },
+            afterFormCreate: (e) => {
+              window.location.reload();
+            },
+            afterFormUpdate: (e) => {
+              window.location.reload();
+            },
+            afterFormCreateError: (e) => {
+              alert('Não foi possível enviar o formulário.')
+            },
+            afterFormUpdateError: (e) => {
+              alert('Não foi possível enviar o formulário.')
+            }, 
+        }, configs.listeners);
+
         this.options = Object.assign({}, {
             formCreate: "#modal-create form",
             formUpdate: "#modal-update form",
@@ -15,57 +36,69 @@ class HcodeGrid {
         this.formCreate = document.querySelector(this.options.formCreate);
 
         this.formCreate.save().then(json => {
-          window.location.reload();
+          this.fireEvent("afterFormCreate");
         }).catch(err => {
+          this.fireEvent('afterFormCreateError');
           console.log(err);
         });
   
         this.formUpdate = document.querySelector(this.options.formUpdate);
   
         this.formUpdate.save().then(json => {
+            this.fireEvent("afterFormUpdate");
           window.location.reload();
         }).catch(err => {
+          this.fireEvent('afterFormUpdateError');
           console.log(err);
         });
     }
 
+    fireEvent(name, args) {
+        if(typeof this.options.listeners[name] === 'function') this.options.listeners[name].apply(this, args)
+    }
+
+    getTrData(e) {
+        let tr = e.srcElement.parentNode.parentNode;
+    
+        return JSON.parse(tr.dataset.row);
+    }
+
     initButtons() {
+        [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
+            btn.addEventListener("click", e => {
+              this.fireEvent('beforeUpdateClick', [e]);
+
+              let data = this.getTrData(e);
+
+              for(let name in data) {
+                let input = this.formUpdate.querySelector(`[name=${name}]`);
+    
+                switch(name) {
+                  case 'date':
+                  if(input) input.value = moment(data[name]).format('YYYY-MM-DD');
+                  break
+                  default:
+                    if(input) input.value = data[name];
+                }
+              }
+            this.fireEvent('afterUpdateClick', [e]);
+            });
+          }); 
+
         [...document.querySelectorAll(this.options.btnDelete)].forEach(btn => {
           btn.addEventListener("click", e => {
-            let tr = e.srcElement.parentNode.parentNode;
-  
-            let data = JSON.parse(tr.dataset.row);
+            this.fireEvent('beforeDeleteClick');
+
+            let data = this.getTrData(e);
   
             if(confirm(eval('`' + this.options.deleteMsg + '`'))) {
               fetch(eval('`' + this.options.deleteUrl + '`'), {
               method: "DELETE"
             }).then(response => response.json()).then(json => {
-              window.location.reload();
+              this.fireEvent('afterDeleteClick');
             });
             }
           });
         });
-  
-        [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
-          btn.addEventListener("click", e => {
-            let tr = e.srcElement.parentNode.parentNode;
-  
-            let data = JSON.parse(tr.dataset.row);
-  
-            for(let name in data) {
-              let input = this.formUpdate.querySelector(`[name=${name}]`);
-  
-              switch(name) {
-                case 'date':
-                if(input) input.value = moment(data[name]).format('YYYY-MM-DD');
-                break
-                default:
-                  if(input) input.value = data[name];
-              }
-            }
-  
-          $("#modal-update").modal("show");
-          });
-        }); 
     }
 }
